8ff342c5e9ab740df316243a1bdaa42c13550117
Andrew Santosa <santosa_1999@yahoo.com>
2018-03-22 17:55:25 +0800

    Iterate over projects in run.sh

--- run.sh
+++ run.sh
@@ -4,12 +4,13 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $SCRIPT_DIR
 
-export PROJECT_NAME=libarchive
-for PROJECT_NUM in `ls -d projects/libarchive_* | sed s/[^[:digit:]]/\ /g`
+for PROJECT_NAME in ffmpeg libarchive
 do
-	export PROJECT_NAME=libarchive
-	sudo python infra/helper.py build_image $PROJECT_NAME $PROJECT_NUM
-	sudo python infra/helper.py build_fuzzers --sanitizer address $PROJECT_NAME $PROJECT_NUM
-	# For running the fuzzer
-	# sudo python infra/helper.py run_fuzzer libarchive_$PROJECT_NUM libarchive_fuzzer	
+	for PROJECT_NUM in `ls -d "projects/$PROJECT_NAME"_* | sed s/[^[:digit:]]/\ /g`
+	do
+		sudo python infra/helper.py build_image $PROJECT_NAME $PROJECT_NUM
+		sudo python infra/helper.py build_fuzzers --sanitizer address $PROJECT_NAME $PROJECT_NUM
+		# For running the fuzzer
+		# sudo python infra/helper.py run_fuzzer libarchive_$PROJECT_NUM libarchive_fuzzer
+	done
 done
