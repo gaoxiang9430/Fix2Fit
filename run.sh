9ca5444efb9a4f74f5bf293fc90430dd25165b1f
Andrew Santosa <santosa_1999@yahoo.com>
2018-03-22 16:34:16 +0800

    Added instruction on how to run the fuzzer in run.sh

--- run.sh
+++ run.sh
@@ -10,4 +10,6 @@ do
 	export PROJECT_NAME=libarchive
 	sudo python infra/helper.py build_image $PROJECT_NAME $PROJECT_NUM
 	sudo python infra/helper.py build_fuzzers --sanitizer address $PROJECT_NAME $PROJECT_NUM
+	# For running the fuzzer
+	# sudo python infra/helper.py run_fuzzer libarchive_$PROJECT_NUM libarchive_fuzzer	
 done
