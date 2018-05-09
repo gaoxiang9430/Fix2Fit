54b29980720978f9f11882ba677bd73500af1da6
Andrew Santosa <santosa_1999@yahoo.com>
2018-05-09 18:53:42 +0800

    Update to build_llvm-3.8.1.sh

--- build_llvm-3.8.1.sh
+++ build_llvm-3.8.1.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 # Build LLVM 3.8.1 for building f1x
 
-pushd build
 mkdir -p llvm-3.8.1
 pushd llvm-3.8.1
-wget http://releases.llvm.org/3.8.1/llvm-3.8.1.src.tar.xz
-wget http://releases.llvm.org/3.8.1/cfe-3.8.1.src.tar.xz
-wget http://releases.llvm.org/3.8.1/compiler-rt-3.8.1.src.tar.xz
+if [ ! -e llvm-3.8.1.src.tar.xz ] ; then
+	wget http://releases.llvm.org/3.8.1/llvm-3.8.1.src.tar.xz
+fi
+if [ ! -e cfe-3.8.1.src.tar.xz ] ; then
+	wget http://releases.llvm.org/3.8.1/cfe-3.8.1.src.tar.xz
+fi
+if [ ! -e compiler-rt-3.8.1.src.tar.xz ] ; then
+	wget http://releases.llvm.org/3.8.1/compiler-rt-3.8.1.src.tar.xz
+fi
 tar xf llvm-3.8.1.src.tar.xz
 tar xf cfe-3.8.1.src.tar.xz
 tar xf compiler-rt-3.8.1.src.tar.xz
@@ -17,8 +22,7 @@ mkdir -p install
 LLVM_INSTALL_DIR=$PWD/install
 mkdir -p build
 pushd build
-cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$LLVM_INSTALL_DIR -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=Off
+cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$LLVM_INSTALL_DIR -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=Off ../src
 make install
-popd
 popd
 popd
