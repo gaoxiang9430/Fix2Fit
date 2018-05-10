91113798affb98b5814ad24be876296d60cf4d17
Andrew Santosa <santosa_1999@yahoo.com>
2018-05-10 11:00:06 +0800

    Updated build_llvm-3.8.1.sh

--- build_llvm-3.8.1.sh
+++ build_llvm-3.8.1.sh
@@ -5,24 +5,24 @@ mkdir -p llvm-3.8.1
 pushd llvm-3.8.1
 if [ ! -e llvm-3.8.1.src.tar.xz ] ; then
 	wget http://releases.llvm.org/3.8.1/llvm-3.8.1.src.tar.xz
+	tar xf llvm-3.8.1.src.tar.xz
+	mv llvm-3.8.1.src src
 fi
 if [ ! -e cfe-3.8.1.src.tar.xz ] ; then
 	wget http://releases.llvm.org/3.8.1/cfe-3.8.1.src.tar.xz
+	tar xf cfe-3.8.1.src.tar.xz
+	mv cfe-3.8.1.src src/tools/clang
 fi
 if [ ! -e compiler-rt-3.8.1.src.tar.xz ] ; then
 	wget http://releases.llvm.org/3.8.1/compiler-rt-3.8.1.src.tar.xz
+	tar xf compiler-rt-3.8.1.src.tar.xz
+	mv compiler-rt-3.8.1.src src/projects/compiler-rt
 fi
-tar xf llvm-3.8.1.src.tar.xz
-tar xf cfe-3.8.1.src.tar.xz
-tar xf compiler-rt-3.8.1.src.tar.xz
-mv llvm-3.8.1.src src
-mv cfe-3.8.1.src src/tools/clang
-mv compiler-rt-3.8.1.src src/projects/compiler-rt
 mkdir -p install
 LLVM_INSTALL_DIR=$PWD/install
 mkdir -p build
 pushd build
 cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$LLVM_INSTALL_DIR -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=Off ../src
-make install
+make -j$(nproc) install
 popd
 popd
